f08442f1aac02f39da4b080cc385e40df4cfa63e
mifferol <marishaaa20027@gmail.com>
2019-09-23 01:03:45 +0300

    Add files via upload

---
+++ lab-01/06.Area/Area.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Average
+{
+    static void Main()
+    {
+        double a;
+        double b;
+        double h;
+        double area;
+
+        Console.Write("input a : ");
+        a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("input b : ");
+        b = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("input h : ");
+        h = Convert.ToDouble(Console.ReadLine());
+
+        area = (a + b) / 2.0 * h;
+
+        Console.WriteLine(area);
+        Console.ReadKey();
+    }
+}
